ca62c84c953f66cf634bb1722122cebb1d9583ba
AMAN CHANDRAVANSHI <146625429+AmanChandra1c@users.noreply.github.com>
2025-11-04 23:29:39 +0530

    Time: 17 ms (10.42%), Space: 47.4 MB (7.35%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,15 +1,25 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-       int[] count = new int[26];
-        for (char c : magazine.toCharArray()) {
-            count[c - 'a']++;
+        HashMap<Character, Integer> hmr = new HashMap<>();
+        HashMap<Character, Integer> hmm = new HashMap<>();
+
+        for(int i = 0; i < ransomNote.length(); i++){
+            char c = ransomNote.charAt(i);
+            hmr.put(c, hmr.getOrDefault(c, 0)+1);
         }
-        for (char c : ransomNote.toCharArray()) {
-            if (--count[c - 'a'] < 0) {
+        
+        for(int i = 0; i < magazine.length(); i++){
+            char c = magazine.charAt(i);
+            hmm.put(c, hmm.getOrDefault(c, 0)+1);
+        }
+
+        for(char i : hmr.keySet()){
+            if(hmm.containsKey(i) && hmm.get(i) >= hmr.get(i)){
+                hmm.remove(i);
+            }else {
                 return false;
             }
         }
-
         return true;
     }
 }
